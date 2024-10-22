764a135e462b12cd0f1d7ff3e63725d2c76fc95f
Tomoyuki Sakurai <y@trombik.org>
2024-10-22 11:32:46 +0700

    bugfix: make gmake verbose during the biuld

--- devel/esp-llvm-embedded-toolchain/files/patch-cmake_esp-gnu-components.cmake
+++ devel/esp-llvm-embedded-toolchain/files/patch-cmake_esp-gnu-components.cmake
@@ -1,4 +1,4 @@
---- cmake/esp-gnu-components.cmake.orig	2024-10-12 05:07:48 UTC
+--- cmake/esp-gnu-components.cmake.orig	2024-08-29 07:46:34 UTC
 +++ cmake/esp-gnu-components.cmake
 @@ -228,7 +228,7 @@ function(
          string(REPLACE "-" ";" var_comps ${target_triple})
@@ -9,3 +9,12 @@
          message(STATUS "Make source dir copy for ${cpu}. ${binutils_SOURCE_DIR} -> ${binutils_src_dir}")
          file(COPY ${binutils_SOURCE_DIR}/ DESTINATION ${binutils_src_dir})
          message(STATUS "Apply Xtensa overlay for ${cpu}. ${xtensa_overlays_SOURCE_DIR}/xtensa_${cpu}/binutils -> ${binutils_src_dir}")
+@@ -248,7 +248,7 @@ function(
+             ${binutils_src_dir}/configure --host=${HOST_TRIPLE} --target=${target_triple} --prefix=<INSTALL_DIR>
+                 --program-prefix=${target_triple}-clang-
+                 ${config_opts}
+-        BUILD_COMMAND make -j${MAKE_JOBS_NUM}
++        BUILD_COMMAND make -j${MAKE_JOBS_NUM} V=1
+         INSTALL_COMMAND make install-strip
+         USES_TERMINAL_CONFIGURE TRUE
+         USES_TERMINAL_BUILD TRUE
