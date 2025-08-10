bd434ba0060c4547bf2154d2ba721854a9e1b5a3
Tomoyuki Sakurai <y@trombik.org>
2025-08-10 15:55:39 +0700

    bugfix: fix git ref of llbm-project

--- lang/rust-esp/files/gh_tuple.sh
+++ lang/rust-esp/files/gh_tuple.sh
@@ -20,7 +20,7 @@ for MODULE_NAME in ${MODULE_NAMES}; do
     MODULE_PATH=`git config --file .gitmodules --get submodule.${MODULE_NAME}.path`
     MODULE_ORG=`git config --file .gitmodules --get submodule.${MODULE_NAME}.url | sed -e 's|https://github.com/||' -e 's|.git$||' | cut -d '/' -f 1`
     MODULE_PROJECT=`git config --file .gitmodules --get submodule.${MODULE_NAME}.url | sed -e 's|https://github.com/||' -e 's|.git$||' | cut -d '/' -f 2`
-    MODULE_SHA=`git submodule status ${MODULE_PATH} | cut -c 2-8`
+    MODULE_SHA=`git submodule status ${MODULE_PATH} | cut -c 2-42`
     MODULE_GROUP=`echo ${MODULE_ORG}_${MODULE_PROJECT} | sed -e 's|-|_|g'`
     MODULE_TUPLE="${MODULE_ORG}:${MODULE_PROJECT}:${MODULE_SHA}:${MODULE_GROUP}/${MODULE_PATH}"
     MODULE_TUPLES="${MODULE_TUPLES} ${MODULE_TUPLE}"
