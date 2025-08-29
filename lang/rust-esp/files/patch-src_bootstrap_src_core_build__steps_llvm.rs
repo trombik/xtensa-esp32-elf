48b9a33a3052da1a3759b2fdefb83de30ca380ba
Tomoyuki Sakurai <y@trombik.org>
2025-08-29 11:34:28 +0700

    update: rust-esp 1.89.0

--- lang/rust-esp/files/patch-src_bootstrap_src_core_build__steps_llvm.rs
+++ lang/rust-esp/files/patch-src_bootstrap_src_core_build__steps_llvm.rs
@@ -1,10 +1,10 @@
---- src/bootstrap/src/core/build_steps/llvm.rs.orig	2024-10-26 00:36:52 UTC
+--- src/bootstrap/src/core/build_steps/llvm.rs.orig	2025-08-20 10:40:37 UTC
 +++ src/bootstrap/src/core/build_steps/llvm.rs
-@@ -727,6 +727,7 @@ fn configure_cmake(
-             .define("CMAKE_ASM_COMPILER", sanitize_cc(&cc));
-     }
+@@ -746,6 +746,7 @@ fn configure_cmake(
+         .define("CMAKE_CXX_COMPILER", sanitize_cc(&cxx))
+         .define("CMAKE_ASM_COMPILER", sanitize_cc(&cc));
  
 +    cfg.build_arg("--verbose").build_arg("--verbose");
      cfg.build_arg("-j").build_arg(builder.jobs().to_string());
-     let mut cflags: OsString = builder
-         .cflags(target, GitRepo::Llvm, CLang::C)
+     // FIXME(madsmtm): Allow `cmake-rs` to select flags by itself by passing
+     // our flags via `.cflag`/`.cxxflag` instead.
